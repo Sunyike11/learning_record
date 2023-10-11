7108a669814121e19d5fb86e9a4c99a6e39327e2
Sunyike11 <120368329+Sunyike11@users.noreply.github.com>
2023-10-11 10:03:18 +0800

    Delete tf3.7 directory

--- tf3.7/learn/loss.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-x = tf.random.normal([1,784])
-w = tf.random.normal([784,2])
-b = tf.zeros([2])
-
-logits = x @ w + b
-loss = tf.losses.categorical_crossentropy([0,1], logits, from_logits=True)
-print(loss)
--- tf3.7/test.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-print(tf.constant(1.)+tf.constant(2.))
-
-print(tf.test.is_gpu_available())
